c9373f67b6b8656f314c3af67945185c985bca9c
engrkashem <kashemaust@gmail.com>
2024-01-16 22:36:21 +0600

    routes and side bar items made dynamically using array.reduce method

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -1,32 +1,33 @@
-import { Layout, Menu, MenuProps } from "antd";
-import { NavLink, Outlet } from "react-router-dom";
+import { Layout, Menu } from "antd";
+import { Outlet } from "react-router-dom";
+import { adminSideBarItems } from "../../routes/adminRoutes";
 
 const { Header, Content, Footer, Sider } = Layout;
 
-const items: MenuProps["items"] = [
-  {
-    key: "Dashboard",
-    label: <NavLink to={"/admin/dashboard"}>Dashboard</NavLink>,
-  },
-  {
-    key: "User management",
-    label: "User management",
-    children: [
-      {
-        key: "Create Admin",
-        label: <NavLink to={"/admin/create-admin"}>create admin</NavLink>,
-      },
-      {
-        key: "Create Student",
-        label: <NavLink to={"/admin/create-student"}>create student</NavLink>,
-      },
-      {
-        key: "Create Faculty",
-        label: <NavLink to={"/admin/create-faculty"}>create faculty</NavLink>,
-      },
-    ],
-  },
-];
+// const items: MenuProps["items"] = [
+//   {
+//     key: "Dashboard",
+//     label: <NavLink to={"/admin/dashboard"}>Dashboard</NavLink>,
+//   },
+//   {
+//     key: "User management",
+//     label: "User management",
+//     children: [
+//       {
+//         key: "Create Admin",
+//         label: <NavLink to={"/admin/create-admin"}>create admin</NavLink>,
+//       },
+//       {
+//         key: "Create Student",
+//         label: <NavLink to={"/admin/create-student"}>create student</NavLink>,
+//       },
+//       {
+//         key: "Create Faculty",
+//         label: <NavLink to={"/admin/create-faculty"}>create faculty</NavLink>,
+//       },
+//     ],
+//   },
+// ];
 
 const MainLayout = () => {
   return (
@@ -58,7 +59,7 @@ const MainLayout = () => {
           theme="dark"
           mode="inline"
           defaultSelectedKeys={["4"]}
-          items={items}
+          items={adminSideBarItems}
         />
       </Sider>
       <Layout>
--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
-import router from "./routes/Routes.tsx";
+import router from "./routes/routes.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>

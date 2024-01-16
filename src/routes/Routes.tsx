d5977c1ddacf7b7e5e1a87b16d7191550e04c89e
engrkashem <kashemaust@gmail.com>
2024-01-16 19:27:42 +0600

    admin layput is no t needed

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -6,7 +6,6 @@ import Login from "../pages/Login";
 import Register from "../pages/Register";
 import AdminDashboard from "../pages/admin/AdminDashboard";
 import CreateStudent from "../pages/admin/CreateStudent";
-import AdminLayout from "../components/layout/AdminLayout";
 
 const router = createBrowserRouter([
   {
@@ -25,7 +24,7 @@ const router = createBrowserRouter([
   },
   {
     path: "/admin",
-    element: <AdminLayout />,
+    element: <App />,
     children: [
       {
         path: "dashboard",

a86d0977032aa63154479281d32293ba67e317da
engrkashem <kashemaust@gmail.com>
2024-03-14 13:02:24 +0600

    create academic semester type error for server response handled. global type terror and tresponse created

--- src/pages/admin/academicManagement/CreateAcademicSemester.tsx
+++ src/pages/admin/academicManagement/CreateAcademicSemester.tsx
@@ -8,6 +8,7 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { academicSemesterSchema } from "../../../schemas/academicManagement.schema";
 import { useAddAcademicSemesterMutation } from "../../../redux/features/admin/academicManagement.api";
 import { toast } from "sonner";
+import { TResponse } from "../../../types/global";
 
 const yearOptions = [0, 1, 2, 3, 5].map((yearInc) => ({
   value: String(currentYear + yearInc),
@@ -18,6 +19,7 @@ const CreateAcademicSemester = () => {
   const [addAcademicSemester] = useAddAcademicSemesterMutation();
 
   const onSubmit: SubmitHandler<FieldValues> = async (data) => {
+    const toastId = toast.loading("Academic Semester is Creating");
     const name = semesterOptions[Number(data?.name) - 1]?.label;
 
     const semesterData = {
@@ -29,10 +31,18 @@ const CreateAcademicSemester = () => {
     };
     try {
       console.log(semesterData);
-      const res = await addAcademicSemester(semesterData);
+      const res = (await addAcademicSemester(semesterData)) as TResponse;
+
+      if (res?.error) {
+        toast.error(res?.error?.data?.message, {
+          id: toastId,
+        });
+      } else {
+        toast.success(res?.data?.message, { id: toastId });
+      }
       console.log(res);
     } catch {
-      toast.error("Something went wrong!");
+      toast.error("Something went wrong!", { id: toastId });
     }
   };
 
---
+++ src/types/global.ts
@@ -0,0 +1,16 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+export type TError = {
+  data?: {
+    message: string;
+    success: boolean;
+    stack: string;
+  };
+  status: number;
+};
+
+export type TResponse = {
+  data?: any;
+  error?: TError;
+  message: string;
+  success: boolean;
+};
